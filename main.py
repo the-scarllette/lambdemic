b24688cde55817e2286eea3ebb8d1bdd4bae4a11
Scarllette Ellis <re412@bath.ac.uk>
2022-03-03 13:37:52 +0000

    added neural net gradient function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from graphics import *
 from qlearning.qlearningagent import QLearningAgent
 from results.resultsmanager import ResultsManager
 from neuralnet.neuralnet import NeuralNet
-'''
+
 # Initialising Window
 width = 1000
 height = 600
@@ -42,15 +42,4 @@ for i in range(num_runs):
     game.run_game()
 
 game.graph_results()
-'''
 
-cols = 3
-inputs = 2
-net = NeuralNet(columns=cols, rows=inputs, net_filename='test_net.json')
-x = [1 for i in range(inputs)]
-y_reg = net.compute_net(x)
-print("Regular computing: " + str(y_reg))
-y_rec = net.compute_net_recursive(x)
-print("Recursive computing: " + str(y_rec))
-print("Are equal true/false: " + str(y_reg == y_rec))
-print("Derivative: " + str(net.gradient(x, 1, 1)))
