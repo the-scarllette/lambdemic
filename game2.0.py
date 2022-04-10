10588831341078cf90fa795bd2ba31c6543ed7cd
Scarllette Ellis <re412@bath.ac.uk>
2022-04-10 12:41:14 +0100

    started building new game environment

---
+++ game2.0.py
@@ -0,0 +1,29 @@
+
+class Game:
+
+    all_colours = ['blue', 'yellow', 'black', 'red']
+
+    def __init__(self, colours=all_colours):
+        return
+
+    # Add get actions
+    def get_action_space(self):
+        return
+
+    # Add get after state
+    def get_after_state(self, action):
+        after_state = None
+        return after_state
+
+    # Add get state space
+    def get_state_space(self):
+        return
+
+    # Add step
+    def step(self, action):
+        next_state, reward, terminal, info = None
+        return next_state, reward, terminal, info
+
+    # Add reset
+    def reset(self):
+        return
